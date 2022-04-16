edf869dc46cb7f19583a0851877174c474447587
shreyash <shreyashyewale84964@gmail.com>
2022-04-16 09:44:35 +0530

    Footer reusable code added,slider swiper of home page and client review changed

--- Travel-Tour/js/script.js
+++ Travel-Tour/js/script.js
@@ -11,30 +11,38 @@ window.onscroll = () =>{
    navbar.classList.remove('active');
 };
 
-var swiper = new Swiper(".home-slider", {
-   loop:true,
-   navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-   },
+var swiper = new Swiper('.home-slider', {
+  autoplay: {
+    delay: 2000
+  },
+  speed: 2000,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 
-var swiper = new Swiper(".reviews-slider", {
-   grabCursor:true,
-   loop:true,
-   autoHeight:true,
-   spaceBetween: 20,
-   breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      700: {
-        slidesPerView: 2,
-      },
-      1000: {
-        slidesPerView: 3,
-      },
-   },
+var swiper = new Swiper('.reviews-slider', {
+  grabCursor: true,
+  loop: true,
+  autoHeight: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2000
+  },
+  speed: 800,
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    700: {
+      slidesPerView: 2
+    },
+    1000: {
+      slidesPerView: 3
+    }
+  }
 });
 
 let loadMoreBtn = document.querySelector('.packages .load-more .btn');
