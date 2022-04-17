25f94fd24021978c52a49263cbcdc2ba8dfa6ead
shreyash <shreyashyewale84964@gmail.com>
2022-04-17 10:07:16 +0530

    Added details to UI after submitting form

--- Travel-Tour/book_details.php
+++ Travel-Tour/book_details.php
@@ -1,5 +1,14 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="css/result.css">
+   <title>Document</title>
+</head>
+<body>
+   <?php
    $servername="localhost";
    $username="root";
    $password="";
@@ -19,7 +28,45 @@
       $request = " insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
       $rs=mysqli_query($connection, $request);
       if($rs){
-         echo "<script>alert('Your Booking Request has been sent successfully')</script>";
+         echo"
+         <div>
+         <table>
+         <tr>
+            <td>Name</td>
+            <td>$name</td>
+         </tr>
+         <tr>
+            <td>Email</td>
+            <td>$email</td>
+         </tr>
+         <tr>
+            <td>Phone</td>
+            <td>$phone</td>
+         </tr>
+         <tr>
+            <td>Address</td>
+            <td>$address</td>
+         </tr>
+         <tr>
+            <td>Location</td>
+            <td>$location</td>
+         </tr>
+         <tr>
+            <td>No of Guests</td>
+            <td>$guests</td>
+         </tr>
+         <tr>
+            <td>Arrival Date</td>
+            <td>$arrivals</td>
+         </tr>
+         <tr>
+            <td>Departure Date</td>
+            <td>$leaving</td>
+         </tr>
+         </table>
+         </div>";
+        echo "<h3>You have successfully submitted your details for enquiry.Our executive will contact you soon!!!</h3>";
+        
       }
       else{
          echo "<script>alert('Your Booking Request has not been sent successfully')</script>";
@@ -30,3 +77,5 @@
    }
 
 ?>
+</body>
+</html>
